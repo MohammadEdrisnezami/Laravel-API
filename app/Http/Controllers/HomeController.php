dc4df6803ce08ce4600bb8061f96e2ba8f3ae802
Mohammad Edris <abdulkarimnezami@gmail.com>
2025-02-27 15:58:05 +0430

    fetch api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use GuzzleHttp\Client;
 class HomeController extends Controller
 {
     //
+    function index(){
+         
+        $client = new Client();
+        $respons=$client->request('GET','https://api.covid19api.com/summary');
+        $respons->getBody();
+
+        return view('index');
+    }
 }
